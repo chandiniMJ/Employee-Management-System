26a5ac32a018041a23044bb89a853a6d65fa1145
Bhakti Bhusari <bhaktibhusari@gmail.com>
2022-02-07 12:19:45 +0530

    SIGN in

--- EMS/src/app/sign-in/sign-in.component.ts
+++ EMS/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,6 +11,19 @@ export class SignInComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  Signinform = new FormGroup({
+    umail: new FormControl("",[Validators.required,Validators.pattern('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')]),
+    upass: new FormControl("",[Validators.required,Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/)])
+  })
+  get umail(){
+    return this.Signinform.get("umail")
+  }
+  get upass(){
+    return this.Signinform.get("upass")
+  }
+   SigninData(){
+    console.log(this.Signinform.value)
   }
 
 }
